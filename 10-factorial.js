336b0e42438454a182c0a4ee54d3fb482488cd1e
Benjamin Barimah-Sarpong <benjaminbarimahsarpong@gmail.com>
2025-05-26 21:47:51 +0000

    added a new file

---
+++ 10-factorial.js
@@ -0,0 +1,10 @@
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = Number(process.argv[2]);
+
+console.log(factorial(isNaN(num) ? 0 : num));
